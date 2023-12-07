c603ee18dc6bc1e4e7bd7d2092b841373f63f0b6
mrkarna <mrk.iitdhn@gmail.com>
2023-12-06 20:49:34 -0500

    rsa algorithm in python

---
+++ rsa.py
@@ -0,0 +1,85 @@
+﻿# Place your imports here
+
+
+class RSA(object):
+    """
+    RSA Encryption/Decryption Algorithm
+
+    Parameters:
+    p, q        : int       prime numbers
+    n           : int       modulus for public&private key
+    phi         : int       lambda(n)
+    e           : int       public key
+    d           : int       private key
+
+    Method:
+    compute_key  : int, int -> None     compute public private key from p, q
+    encrypt     : int -> int            encrypte msg
+    decrypt     : int -> int            decrypt cipher text
+    """
+    p: int
+    q: int
+    n: int
+    phi: int
+    e: int
+    d: int
+
+    def encrypt(self, msg: int) -> int:
+        """Enrypt the message"""
+        # TODO: Implement me
+        return pow(msg, self.e) % self.n
+
+    def decrypt(self, cipher_text: int) -> int:
+        """Decrypt the cipher text"""
+        # TODO: Implement me
+        return pow(cipher_text, self.d) % self.n 
+
+    def compute_key(self, p: int, q: int, coprime_index: int = 0) -> int:
+        """
+        Compute Public&Private Key and assign them to class attribute
+
+        Compute nth coprime according to the coprime index (default to 0)
+        """
+        # TODO: Implement me
+        
+        self.p = p
+
+        self.q = q
+
+        self.n = p*q
+
+        self.phi = (p-1)*(q-1)
+
+        all_coprimes_of_phi = []
+        for i in range (2, self.phi):
+            if(self.find_gcd(self.phi, i) == 1):
+                all_coprimes_of_phi.append(i)
+
+        #assuming coprime_index is always passed and it's 1 based
+        self.e = all_coprimes_of_phi[coprime_index-1]
+
+        #find d (using brute force)
+        self.d = self.find_d(self.e, self.phi)
+
+
+
+
+    def find_gcd(self, p, q):
+        gcd = 1
+        for i in range(1,max(p,q)):
+            if(p%i==0 and q%i==0):
+                gcd = i
+        return gcd
+
+
+    def find_d(self, e, phi):
+
+        for i in range(1, phi):
+            if (i*e)%phi == 1:
+                return i
+
+        return -1
+
+
+
+
---
+++ test.py
@@ -0,0 +1,91 @@
+﻿import os
+from argparse import ArgumentParser
+from typing import TextIO
+
+from rsa import RSA
+
+parser = ArgumentParser(prog="RSA Test", description="RSA Test")
+parser.add_argument(
+    "-i", 
+    "--input",
+    help="input file containing p, q, msg",
+    nargs="?",
+    # default="input.txt",
+    type=lambda x: is_path_valid(
+            arg=x,
+            default_path="input.txt",
+            mode="r",
+        )
+)
+
+def is_path_valid(
+    arg,
+    default_path: str,
+    mode: str,
+    encoding: str = "utf-8",
+):
+    if not os.path.exists(arg):
+        if not os.path.exists(default_path):
+            return 
+        arg = default_path
+        # parser.error(f"The file {arg} does not exist!")
+    return open(arg, mode, encoding=encoding)  # return an open file handle
+
+def read_from_stdin():
+    print("Reading parameters in terminal...")
+    rsa = RSA()
+    while True:
+        try:
+            line = input("Type in order: p q coprime_index msg\n")
+            if line == "" or line == "\n":
+                print("No input detected, terminating...")
+                return
+            p, q, cp_index, msg = line.strip('\n').strip(' ').split(" ")
+            rsa.compute_key(int(p), int(q), int(cp_index))
+            encrypted = rsa.encrypt(int(msg))
+            decrypted = rsa.decrypt(encrypted)
+            print(f"p: {p}, q: {q}, phi: {rsa.phi}, pu: ({rsa.n}, {rsa.e}), pr: ({rsa.n}, {rsa.d}), message: {msg}, encrypted: {encrypted}, decrypted: {decrypted}")
+        except KeyboardInterrupt:
+            return
+        except ValueError as e:
+            print("Error Format. Example input: 67 83 3 24. Detail:", e)
+        except Exception as e:
+            print("Error: ", e)            
+
+
+def read_from_file(file_handler: TextIO):
+    rsa = RSA()
+    p = 0
+    q = 0
+    for index, line in enumerate(file_handler.readlines()):
+        try:
+            if line == "":
+                continue
+            if index % 6 == 0:
+                p, q = line.strip('\n').split(" ")
+                # using third coprime
+                rsa.compute_key(int(p), int(q), 3)
+            else:
+                msg = line.strip('\n')
+                encrypted = rsa.encrypt(int(msg))
+                decrypted = rsa.decrypt(encrypted)
+                print(f"p: {p}, q: {q}, phi: {rsa.phi}, pu: ({rsa.n}, {rsa.e}), pr: ({rsa.n}, {rsa.d}), message: {msg}, encrypted: {encrypted}, decrypted: {decrypted}")
+        except KeyboardInterrupt:
+            return 
+        except ValueError as e:
+            print("Error Format. Should have first line be (p, q) and following five lines be msg. Detail:", e)
+            file_handler.close()
+            return
+        except Exception as e:
+            print("Error:", e)
+
+if __name__ == "__main__":
+    namespace = parser.parse_args()
+
+    file_handler: TextIO = namespace.input
+    if not file_handler:
+        read_from_stdin()
+    else:
+        read_from_file(file_handler)
+    
+        
